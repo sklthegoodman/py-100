16becf8afecaf5881da444493c9f0650da657d9c
wrynnsun <wrynnsun@tencent.com>
2019-09-20 22:02:30 +0800

    get_set

--- class/practise.py
+++ class/practise.py
@@ -51,9 +51,51 @@ def run_clock():
         os.system('clear')
 
 
+'''
+getter和setter
+'''
+class Person:
+    
+    def __init__(self,name,age):
+        self._name = name
+        self._age = age
 
+    # 访问器 - getter方法
+    @property
+    def name(self):
+        return self._name
 
+    # 访问器 - getter方法
+    @property
+    def age(self):
+        return self._age
+
+    # 修复器 - setter方法
+    @age.setter
+    def age(self, value):
+        self._age = value + 10
+
+    def play(self):
+        if self._age <= 16:
+            print('%s正在看熊出没' % self._name)
+        else:
+            print('%s正在看欧冠' % self._name)
+
+def get_set_test():
+    kid = Person('Piggy',12)
+    adult = Person('Pig',22)
+
+    print(kid.name)
+    print(adult.name)
+
+    print('年龄：')
+    print(kid.age)
+    print(adult.age)
+    
+    kid.age = 3
+    print(kid.age)
 
 
 if __name__ == '__main__':
-    run_clock()
+    # run_clock()
+    get_set_test()
